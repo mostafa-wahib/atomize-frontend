8b837cf1224a21ecac117195c9a567ca0d71a7f1
mostafa-wahib <mostafa.wahib@outlook.com>
2022-05-20 23:02:04 -0400

    beginning to refactor css

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import MainShell from "./components/MainShell";
-import { MantineProvider } from "@mantine/core";
+import { MantineProvider, MantineThemeOverride } from "@mantine/core";
 function App() {
+  const theme: MantineThemeOverride = {
+    fontFamily: "Red Hat Display",
+    colors: {
+      accent: ["hsl(248, 53%, 58%)"],
+    },
+  };
   return (
-    <MantineProvider theme={{ fontFamily: "Red Hat Display" }}>
+    <MantineProvider theme={theme}>
       <MainShell />
     </MantineProvider>
   );
--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -1,5 +1,10 @@
+import { createStyles } from "@mantine/core";
+const useStyles = createStyles((theme) => ({
+  wrapper: { color: theme.colors.accent[0] },
+}));
 function MainPage() {
-  return <div></div>;
+  const { classes } = useStyles();
+  return <div className={classes.wrapper}></div>;
 }
 
 export default MainPage;
