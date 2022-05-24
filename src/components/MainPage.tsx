6374c21511dcb848a044c37a5ee3c9b710b60506
Mostafa Wahib <moso@pop-os.localdomain>
2022-05-23 22:00:48 -0400

    small markup

--- src/components/MainPage.tsx
+++ src/components/MainPage.tsx
@@ -1,10 +1,20 @@
-import { createStyles } from "@mantine/core";
-const useStyles = createStyles((theme) => ({
-  wrapper: { color: theme.colors.accent[0] },
+import { Center, createStyles, Grid, Title, Text } from "@mantine/core";
+const useStyle = createStyles((theme) => ({
+  wrapper: { alignItems: "stretch", height: "100%" },
 }));
 function MainPage() {
-  const { classes } = useStyles();
-  return <div className={classes.wrapper}></div>;
+  const { classes } = useStyle();
+  return (
+    <Grid className={classes.wrapper}>
+      <Grid.Col span={6}>
+        <Center>
+          <Title order={1}>Shorten.Track.Win</Title>
+        </Center>
+        <Text size="xl"> Test</Text>
+      </Grid.Col>
+      <Grid.Col span={6}>Test</Grid.Col>
+    </Grid>
+  );
 }
 
 export default MainPage;
