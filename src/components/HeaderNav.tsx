dec9dea732036fd86728e2e6f58104e9784e41b2
mostafa-wahib <mostafa.wahib@outlook.com>
2022-06-13 16:27:56 -0400

    working on burger and header responsive design

--- src/components/HeaderNav.tsx
+++ src/components/HeaderNav.tsx
@@ -1,4 +1,4 @@
-import { Header, MediaQuery, Burger } from "@mantine/core";
+import { Header, Burger } from "@mantine/core";
 import Nav from "./Nav";
 import classes from "../styles/HeaderNav.module.scss";
 interface Props {
@@ -9,9 +9,11 @@ function HeaderNav({ opened, setOpened }: Props) {
   return (
     <Header className={classes.header} height={60} py="md">
       <Nav />
-      <MediaQuery largerThan="sm" styles={{ display: "none" }}>
-        <Burger opened={opened} onClick={() => setOpened((o) => !o)} />
-      </MediaQuery>
+      <Burger
+        className={classes.burger}
+        opened={opened}
+        onClick={() => setOpened((o) => !o)}
+      />
     </Header>
   );
 }
