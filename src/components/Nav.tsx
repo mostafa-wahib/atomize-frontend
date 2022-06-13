abc2c62880a14b279622c7f3de035300757ff986
mostafa-wahib <mostafa.wahib@outlook.com>
2022-06-13 19:35:05 -0400

    transitioning css

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,40 +2,19 @@ import { Box, createStyles, MediaQuery } from "@mantine/core";
 import { useContext } from "react";
 import { Link } from "react-router-dom";
 import { UserContext, UserContextInterface } from "../context/UserContext";
-const useStyles = createStyles((theme) => ({
-  "header-nav": {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    gap: "1em",
-    "& a": {
-      textDecoration: "none",
-      color: theme.colors.accent[1],
-      fontSize: theme.other.fs.sm,
-      [`@media (min-width: ${theme.breakpoints.md}px)`]: {
-        fontSize: theme.other.fs.md,
-      },
-    },
-    //[`@media (max-width: ${theme.breakpoints.sm}px)`]: {
-    //  display: "none",
-    //},
-  },
-}));
+import classes from "../styles/Nav.module.scss";
 function Nav() {
   const { userData }: UserContextInterface = useContext(UserContext);
-  const { classes } = useStyles();
   return (
-    <MediaQuery smallerThan="sm" styles={{ display: "none" }}>
-      <Box className={classes["header-nav"]}>
-        <Link to="/">Home</Link>
-        {!userData.loggedIn && (
-          <Link style={{ marginLeft: "auto" }} to="/login">
-            Login
-          </Link>
-        )}
-        {!userData.loggedIn && <Link to="/register">Register</Link>}
-      </Box>
-    </MediaQuery>
+    <Box className={classes["header-nav"]}>
+      <Link to="/">Home</Link>
+      {!userData.loggedIn && (
+        <Link style={{ marginLeft: "auto" }} to="/login">
+          Login
+        </Link>
+      )}
+      {!userData.loggedIn && <Link to="/register">Register</Link>}
+    </Box>
   );
 }
 
